61e01aefbfb2f1dda5f91b5820cebceb3b28a5d7
Zolastic <honhattienlul@gmail.com>
2023-11-18 18:02:24 +0800

    feat: Added responses when form is initially created

--- src/server/api/routers/forms.ts
+++ src/server/api/routers/forms.ts
@@ -8,12 +8,26 @@ export const formsRouter = createTRPCRouter({
   }),
 
   create: protectedProcedure.mutation(async ({ ctx }) => {
-    return await ctx.db.form.create({
+    const form = await ctx.db.form.create({
       data: {
         userId: ctx.session.user.id,
         name: "Untitled",
         description: "",
       },
     });
+
+    for (let i = 0; i < 7; i++) {
+      await ctx.db.response.create({
+        data: {
+          formId: form.id,
+          questionId: i + 1,
+          questionType: "text",
+          response: "",
+        },
+      });
+    }
+
+    return form;
   }),
 });
+``;
